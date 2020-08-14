9517954fa29e1f92cc56e0f5dc264671766771d6
jotasugiyama <c0117165b3@edu.teu.ac.jp>
2020-08-14 17:22:34 +0900

    [add] 画像圧縮用のファイルアップロード

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require("gulp");
+// 画像を圧縮するプラグインの読み込み
+const imagemin = require("gulp-imagemin");
+const mozjpeg = require("imagemin-mozjpeg");
+const pngquant = require("imagemin-pngquant");
+const changed = require("gulp-changed");
+
+// srcImgフォルダのjpg,png画像を圧縮して、distImgフォルダに保存する
+gulp.task("default", function() {
+    return gulp
+    .src("./save_image/*") // srcImgフォルダ以下のpng,jpg画像を取得する
+    .pipe(changed("distImg")) // srcImg と distImg を比較して異なるものだけ圧縮する
+    .pipe(
+        imagemin([
+            pngquant({
+                quality: [ 0.65, 0.8 ], // 文字列から配列型に変更
+                speed: 1 // スピード
+            }),
+            mozjpeg({
+                quality: 75, // 画質
+                progressive: true
+            }),
+        ])
+    )
+    .pipe(gulp.dest("./compressed/")); //保存
+});
